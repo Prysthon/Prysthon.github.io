4221fab87a004f5ced770425a584ac6ad855f4d6
Tiago Prysthon <tiagoprysthon14@gmail.com>
2025-03-26 20:55:06 -0300

    reformulacao home

--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -1,37 +1,29 @@
 import React from 'react';
 import '../styles/Home.css';
-
-// Importando ícones dos frameworks
 import { DiPython, DiReact, DiNodejsSmall, DiDocker, DiMysql } from 'react-icons/di';
 
-function Hero() {
+function Home() {
   return (
-    <section id="hero">
-      {/* Camada de fundo */}
-      <div className="hero-background"></div>
-      <div className="container hero-content">
-        <div className="hero-text">
-          <p className="hero-small">Olá Sou</p>
-          <h1>Tiago Prysthon</h1>
-          <p className="hero-medium">Estudante de Engenharia da Computação</p>
-          <p className="hero-small">
-            Desenvolvedor Full Stack e estagiário de inteligência competitiva do iFood
-          </p>
-          {/* Seção de ícones dos frameworks */}
-          <div className="tech-icons">
-            <DiPython size={40} title="Python" />
-            <DiReact size={40} title="React" />
-            <DiNodejsSmall size={40} title="Node.js" />
-            <DiDocker size={40} title="Docker" />
-            <DiMysql size={40} title="MySQL" />
-          </div>
+    <section id="home">
+      <div className="hero-left">
+        <p className="hero-small">Olá, Sou</p>
+        <h1>Tiago Prysthon</h1>
+        <p className="hero-medium">Estudante de Engenharia da Computação</p>
+        <p className="hero-small">
+          Desenvolvedor Full Stack e estagiário de inteligência competitiva do iFood
+        </p>
+        <div className="tech-icons">
+          <DiPython size={40} title="Python" />
+          <DiReact size={40} title="React" />
+          <DiNodejsSmall size={40} title="Node.js" />
+          <DiDocker size={40} title="Docker" />
+          <DiMysql size={40} title="MySQL" />
         </div>
-        <div className="hero-image">
-          <img src="photo.jpeg" alt="Foto de Tiago Prysthon" />
-        </div>
+      </div>
+      <div className="hero-right">
       </div>
     </section>
   );
 }
 
-export default Hero;
+export default Home;
