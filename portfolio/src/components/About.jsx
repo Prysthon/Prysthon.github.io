8380c89c003fa11b97959556fb0d8b8ca73cfacf
Tiago Prysthon <tiagoprysthon14@gmail.com>
2025-03-29 19:21:54 -0300

    fix about

--- portfolio/src/components/About.jsx
+++ portfolio/src/components/About.jsx
@@ -1,17 +1,69 @@
 import React from 'react';
 import '../styles/About.css';
+import { MdWorkOutline } from "react-icons/md";
+import { FaBook } from "react-icons/fa";
+import { FaLanguage } from "react-icons/fa6";
+import { RiComputerLine } from "react-icons/ri";
 
 function About() {
   return (
     <section id="about">
-      <div className="container content">
+      <div>
         <h2>Sobre Mim</h2>
-        <p>
-          Olá, sou Tiago Prysthon, estudante do 6° semestre de Engenharia da Computação no Senai Cimatec
-          e Desenvolvedor Web Full Stack formado pela Trybe. Possuo experiência com Python, React Native,
-          Node.js e outras tecnologias, atuando tanto no front-end quanto no back-end.
-        </p>
-        <p>Confira abaixo minhas experiências e projetos.</p>
+        <div className="about-content">
+          <div className="about-text">
+            <p>
+            Durante meu percurso acadêmico e profissional, adquiri uma base sólida em uma variedade de linguagens de programação, incluindo Python, C++, Javascript, Typescript e Java, bem como em HTML, CSS e SQL. Tenho experiência em construir aplicações web usando React e seus Hooks. Além disso, desenvolvi projetos voltados para o back-end usando Node.js, Express e banco de dados, como MySQL e MongoDB, com o uso de ORM's como Sequelize e Mongoose.
+            </p>
+            <p>Confira abaixo minhas experiências e projetos.</p>
+          </div>
+          <div className="about-info">
+            <table>
+              <tbody>
+                <tr>
+                  <td>
+                    <div className="info-item">
+                      <FaBook size={24} />
+                      <div>
+                        <strong>Educação:</strong>
+                        <span> Graduando Engenharia da Computação</span>
+                      </div>
+                    </div>
+                  </td>
+                  <td>
+                    <div className="info-item">
+                      <MdWorkOutline size={24} />
+                      <div>
+                        <strong>Empresa:</strong>
+                        <span> iFood</span>
+                      </div>
+                    </div>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <div className="info-item">
+                      <FaLanguage size={24} />
+                      <div>
+                        <strong>Línguas:</strong>
+                        <span> Português / Inglês</span>
+                      </div>
+                    </div>
+                  </td>
+                  <td>
+                    <div className="info-item">
+                      <RiComputerLine size={24} />
+                      <div>
+                        <strong>Interesses:</strong>
+                        <span> Data science, Business Intelligence, A.I.</span>
+                      </div>
+                    </div>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     </section>
   );
